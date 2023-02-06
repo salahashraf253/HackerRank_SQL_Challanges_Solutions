fe59de336bcd4f35327d424456d308f4d5bdee55
Salah Ashraf <salahashraf924@gmail.com>
2023-02-06 19:11:39 +0200

    Update Select_By_Id.sql

--- Basic Select/Select_By_Id.sql
+++ Basic Select/Select_By_Id.sql
@@ -1,3 +1,3 @@
-// link: hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+# link: hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
 
 SELECT * FROM city where id = 1661;
